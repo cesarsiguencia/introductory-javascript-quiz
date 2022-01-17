24b33b4163ede79753585840c500bccd734b5f58
cesarsiguencia <cesar.siguencia@gmail.com>
2022-01-16 23:12:43 -0500

    could not attempt to complete working project, final upload

--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,7 @@ var introductionDirections = document.querySelector("#introduction-box")
 var startBtn = document.querySelector("#start-button");
 var optionsBtns = document.getElementsByClassName("button-style");
 var footerBox = document.querySelector("footer");
+var counter = 6;
 
 
 var questionsArray = [
@@ -15,31 +16,47 @@ var questionsArray = [
         {text: 'console.log', correct: true},
         {text: 'message.log', correct: false},
         {text: 'querySelector', correct: false},
-        {text: 'event.targe', correct: false},
+        {text: 'event.target', correct: false},
         ]
     },
 
     {
     question: "What does the acronym 'DOM' stand for?",
     answers: [
-        {text: '1', correct: true},
-        {text: '2', correct: false},
-        {text: '3', correct: false},
-        {text: '4', correct: false},
+        {text: 'Document Objective Module', correct: false},
+        {text: 'Document Object Model', correct: true},
+        {text: 'Dominant Order Model', correct: false},
+        {text: 'Dominant Object Mass', correct: false},
         ]
+    },
 
+    {
+    question: "What is the JavaScript command to create a attach a newly create element to an existing declared variable?",
+    answers: [
+        {text: 'pasteElement', correct: false},
+        {text: 'glueChild', correct: false},
+        {text: 'appendChild', correct: true},
+        {text: 'appendElement', correct: false},
+        ]
+    },
 
-
-    }
+    {
+    question: "What is the different between a JavaScript 'click' vs 'submit' command in addEventLister?",
+    answers: [
+        {text: 'Nothing, they accomplish the same thing', correct: false},
+        {text: 'Click is for Enter keyboards presses while Submit is for mouse clicks', correct: false},
+        {text: 'Click refers to mouse clicks while Submit refers to submitting a form', correct: true},
+        {text: 'Click refers to starting a function while Submit executes it', correct: false},
+        ]
+    },
 ]
 
 var questionsOrder = 0
 var shuffledQuestions = questionsArray.sort(() => Math.random() - .5);
-   
-    // "What is the JavaScript command to create a attach a newly create element to an existing declared variable?",
-    // "What is the different between a JavaScript 'click' vs 'submit' command?",
+   // ,
     // "When setting a timer in JavaScript, what units are used?",
     // "What should you type to create a dynamic HTML element?",
+    
     // "What equation is used to declare BOTH conditions must be met in a condition statement?",
     // "What would you type in the console to get the result for '10+2'?"
 
@@ -58,7 +75,7 @@ var startQuiz = function(){
 
     var timer = document.createElement("div")
     timer.className = "footer-style"
-    timer.innerHTML = "TIME REMAINING: ";
+    timer.innerHTML = "TIME REMAINING:" + counter--;
 
     footerBox.appendChild(timer);
 
@@ -77,6 +94,8 @@ var individualQuestions = function(question){
     // }
 
 
+  
+
     var quizQuestions = document.createElement("div");
     quizQuestions.className = "first-box-style";
     quizQuestions.innerHTML = "<h2 class='first-text-style'>" + question.question + "</H2>";
@@ -88,13 +107,13 @@ var individualQuestions = function(question){
 
     })
 
+    // nextQuestion(quizQuestions);
+
 }
 
 var chooseAnswer = function(answer){
 
-    for (var i=0; i < answer.length; i++){
-        console.log(answer[i]);
-    }
+   
 
     var quizOptions = document.createElement("div");
     quizOptions.className = "answers-style";
@@ -116,8 +135,32 @@ var chooseAnswer = function(answer){
     // var optionsBtns = querySelector(".button-style");
 
     // nextQuestion(optionsBtns);
+   
+    optionBtn1.addEventListener("click", nextQuestion)
 
+    // nextQuestion(quizOptions);
+    
 }
+
+var nextQuestion = function(quizQuestions){
+
+    // quizQuestions.remove();
+    
+    
+
+    // for (var i=0; i < questionsArray.length; i++){
+    //     console.log(questionsArray[i]);
+        
+        
+    //     }
+        startQuiz()
+}
+
+
+    
+    
+
+
 
 // optionsBtns.addEventListener("click") {
 //     individualQuestions();
@@ -170,9 +213,23 @@ var quizEnded = function(){
 
 
 var quizTimer = function(){
+
+   
+
     setTimeout(quizEnded, 6000);
     setTimeout(playerDetails, 6000);
+
+    var startCountdown = setInterval(quizTimer, 1000)
+
+
 }
+
+
+
+
+
+
 
 startBtn.addEventListener("click",startQuiz);
 startBtn.addEventListener("click",quizTimer);
+
